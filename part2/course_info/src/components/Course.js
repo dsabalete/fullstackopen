a6e4cb67579a8703d92719ab1b6ccbc4fa1b55f6
David Sabalete <dsabalete@gmail.com>
2021-02-24 22:23:47 +0100

    feat: 2.2 course information step7

--- part2/course_info/src/components/Course.js
+++ part2/course_info/src/components/Course.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
-const Course = ({ course }) => (
-    <div>
-        <Header name={course.name} />
-        <Content parts={course.parts} />
-    </div>
-)
+const Course = ({ course }) => {
+    const total = course.parts
+        .map((part) => part.exercises)
+        .reduce((acc, curr) => acc + curr)
+
+    return (
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total total={total} />
+        </div>
+    )
+}
 
 export default Course
---
+++ part2/course_info/src/components/Total.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Total = ({ total }) => <strong>Total of {total} exercises</strong>
+
+export default Total
