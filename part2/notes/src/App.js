3c1519e6a5b1c203dc664e452522b73e12da8d5b
David Sabalete <dsabalete@gmail.com>
2021-02-27 21:28:01 +0100

    feat: promises and errors

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -8,8 +8,10 @@ const App = () => {
     const [showAll, setShowAll] = useState(true)
 
     useEffect(() => {
-        noteService.getAll().then((initialNotes) => {
-            setNotes(initialNotes)
+        noteService.getAll().then((response) => {
+            setNotes(response)
+            // .then((initialNotes) => {
+            // setNotes(initialNotes)
         })
     }, [])
 
@@ -17,11 +19,19 @@ const App = () => {
         const note = notes.find((n) => n.id === id)
         const changedNote = { ...note, important: !note.important }
 
-        noteService.update(id, changedNote).then((returnedNote) => {
-            setNotes(
-                notes.map((note) => (note.id !== id ? note : returnedNote))
-            )
-        })
+        noteService
+            .update(id, changedNote)
+            .then((returnedNote) => {
+                setNotes(
+                    notes.map((note) => (note.id !== id ? note : returnedNote))
+                )
+            })
+            .catch((error) => {
+                alert(
+                    `the note '${note.content}' was already deleted from server`
+                )
+                setNotes(notes.filter((n) => n.id !== id))
+            })
     }
 
     const addNote = (event) => {
