f9274bd47079d9fca540cffa59135055f9522e62
David Sabalete <dsabalete@gmail.com>
2021-03-02 22:26:09 +0100

    feat: 3.5 phonebook backend step5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -54,6 +54,30 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+const generateId = () => {
+    return Math.floor(Math.random() * Math.floor(10 ** 10))
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
