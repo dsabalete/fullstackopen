410f4689665d5d461d8782412d09f1070c92c9b3
David Sabalete <dsabalete@gmail.com>
2021-03-07 20:53:35 +0100

    feat: add error handling

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -58,9 +58,18 @@ app.post('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then((note) => {
-        response.json(note.toJSON())
-    })
+    Note.findById(request.params.id)
+        .then((note) => {
+            if (note) {
+                response.json(note.toJSON())
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            response.status(400).send({ error: 'malformatted id' })
+        })
 })
 
 app.put('/api/notes/:id', (request, response) => {
