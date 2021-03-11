aedc9c0a4a501817260b5311feb22d80736dde45
David Sabalete <dsabalete@gmail.com>
2021-03-11 20:31:54 +0100

    feat: add eslint to notes-backend project

--- part3/notes-backend/mongo.js
+++ part3/notes-backend/mongo.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log(
-        'Please provide the password as an argument: node mongo.js <password>'
-    )
-    process.exit(1)
+  console.log(
+    'Please provide the password as an argument: node mongo.js <password>'
+  )
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -12,25 +12,25 @@ const password = process.argv[2]
 const url = `mongodb+srv://fullstack:${password}@cluster0.9aajn.mongodb.net/note-app?retryWrites=true&w=majority`
 
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 })
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean
+  content: String,
+  date: Date,
+  important: Boolean
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-    content: 'HTML is Easy',
-    date: new Date(),
-    important: true
-})
+// const note = new Note({
+//   content: 'HTML is Easy',
+//   date: new Date(),
+//   important: true
+// })
 
 // note.save().then((result) => {
 //     console.log('note saved!')
@@ -38,8 +38,8 @@ const note = new Note({
 // })
 
 Note.find({ important: true }).then((result) => {
-    result.forEach((note) => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+  result.forEach((note) => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
