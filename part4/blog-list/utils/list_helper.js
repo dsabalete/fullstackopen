5562d5c9aaf82d76ed48636bc361edbff0a6f877
David Sabalete <dsabalete@gmail.com>
2021-03-12 13:13:33 +0100

    feat: 4.5 helper functions and unit tests, step3

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -4,7 +4,12 @@ const totalLikes = (blogs) => {
   return blogs.map((blog) => blog.likes).reduce((acc, curr) => acc + curr, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((prev, curr) => (prev.likes > curr.likes ? prev : curr))
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
