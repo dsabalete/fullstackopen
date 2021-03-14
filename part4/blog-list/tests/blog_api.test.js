efabd8c14083ed705a520675a27dad3cffddd58c
David Sabalete <dsabalete@gmail.com>
2021-03-14 10:18:05 +0100

    feat: 4.12 Blog list tests, step5

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -72,6 +72,18 @@ describe('Blog API', () => {
     expect(response.body.likes).toBeDefined()
     expect(response.body.likes).toBe(0)
   })
+
+  test('an HTTP POST request with missing fields returns a 400 BAD REQUEST response', async () => {
+    const blogIncomplete = {
+      author: 'David Sabalete',
+      likes: 0
+    }
+
+    await api.post('/api/blogs').send(blogIncomplete).expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
 afterAll(() => {
