146e9d052215b2abf647827def318321ff47e9ea
David Sabalete <dsabalete@gmail.com>
2021-03-18 21:14:03 +0100

    feat: update blog model

--- part4/blog-list/models/blog.js
+++ part4/blog-list/models/blog.js
@@ -4,7 +4,11 @@ const blogSchema = new mongoose.Schema({
   title: { type: String, required: true, minlength: 5 },
   author: String,
   url: { type: String, required: true, minlength: 8 },
-  likes: { type: Number, default: 0 }
+  likes: { type: Number, default: 0 },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
