fe30fc1ef2b6fe4609e74510599303f6bbbe6c20
David Sabalete <dsabalete@gmail.com>
2021-03-24 11:46:50 +0100

    test: create new user successfuly

--- part4/notes-backend/tests/user.test.js
+++ part4/notes-backend/tests/user.test.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
-const helper = require('./test_helper')
+const { usersInDb } = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const bcrypt = require('bcrypt')
@@ -29,7 +29,7 @@ describe('when there is initially one user at db', () => {
   })
 
   test('creation succeeds with a fresh username', async () => {
-    const usersAtStart = await helper.usersInDb()
+    const usersAtStart = await usersInDb()
 
     const newUser = {
       username: 'mluukkai',
@@ -40,10 +40,10 @@ describe('when there is initially one user at db', () => {
     await api
       .post('/api/users')
       .send(newUser)
-      .expect(200)
+      .expect(201)
       .expect('Content-Type', /application\/json/)
 
-    const usersAtEnd = await helper.usersInDb()
+    const usersAtEnd = await usersInDb()
     expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
 
     const usernames = usersAtEnd.map((u) => u.username)
@@ -51,7 +51,7 @@ describe('when there is initially one user at db', () => {
   })
 
   test('creation fails with proper statuscode and message if username already taken', async () => {
-    const usersAtStart = await helper.usersInDb()
+    const usersAtStart = await usersInDb()
 
     const newUser = {
       username: 'root',
@@ -67,7 +67,7 @@ describe('when there is initially one user at db', () => {
 
     expect(result.body.error).toContain('`username` to be unique')
 
-    const usersAtEnd = await helper.usersInDb()
+    const usersAtEnd = await usersInDb()
     expect(usersAtEnd).toHaveLength(usersAtStart.length)
   })
 })
