b4f9f856da9539b50ad4aadf4fe86f4bb84f53be
David Sabalete <dsabalete@gmail.com>
2021-04-04 20:52:26 +0200

    feat: update login controller

--- part4/notes-backend/controllers/login.js
+++ part4/notes-backend/controllers/login.js
@@ -7,7 +7,6 @@ loginRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
   const user = await User.findOne({ username })
-
   const passwordCorrect =
     user === null ? false : await bcrypt.compare(password, user.passwordHash)
 
@@ -22,10 +21,7 @@ loginRouter.post('/', async (request, response) => {
     id: user._id
   }
 
-  // token expires in 60*60 seconds, that is, in one hour
-  const token = jwt.sign(userForToken, process.env.SECRET, {
-    expiresIn: 60 * 60
-  })
+  const token = jwt.sign(userForToken, process.env.SECRET)
 
   response.status(200).send({ token, username: user.username, name: user.name })
 })
