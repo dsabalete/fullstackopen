9b77187fd4a73b8fd1a3060bc9089ab4e8b903c5
David Sabalete <dsabalete@gmail.com>
2021-04-05 20:14:25 +0200

    feat: 4.15 expand blog list

--- part4/blog-list/controllers/users.js
+++ part4/blog-list/controllers/users.js
@@ -3,28 +3,33 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
-  const { body } = request
+  const { username, name, password } = request.body
 
-  if (!body.password || body.password.length < 3) {
+  if (!password || password.length < 3) {
     return response.status(400).json({ error: 'password missing or invalid' })
   }
 
   const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+  const passwordHash = await bcrypt.hash(password, saltRounds)
 
   const user = new User({
-    username: body.username,
-    name: body.name,
+    username,
+    name,
     passwordHash
   })
 
   const savedUser = await user.save()
 
-  response.json(savedUser)
+  response.status(201).json(savedUser)
 })
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1
+  })
   response.json(users)
 })
 
