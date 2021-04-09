660e9b19a3d1f3095f6234ccfe9a0ddeeafabc04
David Sabalete <dsabalete@gmail.com>
2021-04-09 22:40:53 +0200

    feat: 5.1 blog-list frontend step1

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react'
+import blogService from './services/blogs'
+import loginService from './services/login'
+import Blog from './components/Blog'
+import Notification from './components/Notification'
+import LoginForm from './components/LoginForm'
+
+const App = () => {
+    const [blogs, setBlogs] = useState([])
+    const [errorMessage, setErrorMessage] = useState(null)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        blogService.getAll().then((blogs) => setBlogs(blogs))
+    }, [])
+
+    // useEffect(() => {
+    //     const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    //     if (loggedUserJSON) {
+    //         const user = JSON.parse(loggedUserJSON)
+    //         setUser(user)
+    //         blogService.setToken(user.token)
+    //     }
+    // }, [])
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+
+        try {
+            const user = await loginService.login({ username, password })
+
+            // window.localStorage.setItem(
+            //     'loggedBlogappUser',
+            //     JSON.stringify(user)
+            // )
+            blogService.setToken(user.token)
+            setUser(user)
+            setUsername('')
+            setPassword('')
+        } catch (exception) {
+            setErrorMessage('Wrong credentials')
+            setUser(null)
+            setTimeout(() => {
+                setErrorMessage(null)
+            }, 5000)
+        }
+    }
+
+    const handleLogout = (event) => {
+        event.preventDefault()
+        // window.localStorage.removeItem('loggedBlogappUser')
+        setUser(null)
+    }
+
+    if (user === null) {
+        return (
+            <div>
+                <h2>Log into the Blog-list app</h2>
+                <LoginForm
+                    onSubmit={handleLogin}
+                    username={username}
+                    password={password}
+                    setUsername={setUsername}
+                    setPassword={setPassword}
+                />
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <h2>Blogs</h2>
+            <button onClick={handleLogout}>log out</button>
+            <Notification message={errorMessage} />
+            {blogs.map((blog) => (
+                <Blog key={blog.id} blog={blog} />
+            ))}
+        </div>
+    )
+}
+
+export default App
---
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+// eslint-disable-next-line no-unused-vars
+let token = null
+
+const setToken = (newToken) => {
+    token = `bearer ${newToken}`
+}
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then((response) => response.data)
+}
+
+const blogService = {
+    getAll,
+    setToken
+}
+export default blogService
