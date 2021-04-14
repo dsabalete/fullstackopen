a28c2d8ed35938eea63d62027bdc895a312b3c18
David Sabalete <dsabalete@gmail.com>
2021-04-14 10:27:11 +0200

    feat: 5.9 blog list frontend step9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -131,9 +131,11 @@ const App = () => {
 
             <br />
 
-            {blogs.map((blog) => (
-                <Blog key={blog.id} blog={blog} likeBlog={handleLikeBlog} />
-            ))}
+            {blogs
+                .sort((a, b) => b.likes - a.likes)
+                .map((blog) => (
+                    <Blog key={blog.id} blog={blog} likeBlog={handleLikeBlog} />
+                ))}
         </div>
     )
 }
