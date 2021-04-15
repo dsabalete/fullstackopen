d5f0134b2b48a2bd8fa0071c42948eac6bddfbe5
David Sabalete <dsabalete@gmail.com>
2021-04-15 17:11:12 +0200

    feat: 5.14 blog list tests step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 
-describe('<Blog />', () => {
+describe('Blog', () => {
   let component
   const blog = {
     title: 'A wonderful blog title',
     author: 'Best seller author',
     url: 'https://best.blog.ever',
-    likes: 1000
+    likes: 1000,
+    user: {
+      name: 'Pepe',
+      id: '12345'
+    }
   }
   const likeBlog = jest.fn()
   const removeBlog = jest.fn()
@@ -33,5 +38,15 @@ describe('<Blog />', () => {
     expect(div).toHaveTextContent(blog.author)
     expect(div).not.toHaveTextContent(blog.url)
     expect(div).not.toHaveTextContent(blog.likes)
+  })
+
+  test("the blog's url and number of likes are shown when the button controlling the shown details has been clicked.", () => {
+    const div = component.container.querySelector('.blogDiv')
+    const button = component.getByText('view')
+    fireEvent.click(button)
+    // console.log(prettyDOM(div))
+
+    expect(div).toHaveTextContent(blog.url)
+    expect(div).toHaveTextContent(blog.likes)
   })
 })
