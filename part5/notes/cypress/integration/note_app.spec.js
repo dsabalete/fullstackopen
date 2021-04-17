231176d3d819fd755bd7f3f17cf3a4991a3a9e01
David Sabalete <dsabalete@gmail.com>
2021-04-17 17:22:07 +0200

    test: new note test

--- part5/notes/cypress/integration/note_app.spec.js
+++ part5/notes/cypress/integration/note_app.spec.js
@@ -3,23 +3,36 @@ describe('Note app', function () {
     cy.visit('http://localhost:3000')
   })
 
-  it('front page can be opened', function () {
-    cy.contains('Notes')
-    cy.contains(
-      'Note app, Department of Computer Science, University of Helsinki 2021'
-    )
+  describe('before log in', () => {
+    it('front page can be opened', function () {
+      cy.contains('Notes')
+      cy.contains(
+        'Note app, Department of Computer Science, University of Helsinki 2021'
+      )
+    })
+
+    it('login form can be opened', function () {
+      cy.contains('log in').click()
+    })
   })
 
-  it('login form can be opened', function () {
-    cy.contains('log in').click()
-  })
+  describe('when logged in', function () {
+    beforeEach(function () {
+      cy.contains('log in').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('opensesame')
+      cy.get('#login-button').click()
+    })
 
-  it('user can login', function () {
-    cy.contains('log in').click()
-    cy.get('#username').type('mluukkai')
-    cy.get('#password').type('opensesame')
-    cy.get('#login-button').click()
+    it('user can log in', function () {
+      cy.contains('Matti Luukkainen logged in')
+    })
 
-    cy.contains('Matti Lukkainen logged in')
+    it('a new note can be created', function () {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
   })
 })
