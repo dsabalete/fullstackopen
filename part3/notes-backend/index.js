f0f235fdc21e3b91755ebd9ae0933d9c87890a36
David Sabalete Rodríguez <dsabalete@gmail.com>
2023-07-23 01:41:43 +0200

    verify frontent and backend integration part3-4

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -41,22 +41,18 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId()
-  }
+    important: body.important || false
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then((savedNote) => {
+    response.json(savedNote)
+  })
 })
 
 app.get('/api/notes', (request, response) => {
@@ -73,14 +69,9 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find((note) => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 const PORT = process.env.PORT
