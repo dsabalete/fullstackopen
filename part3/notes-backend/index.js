998c2319656243ad9c109ea814e50da4a0252906
David Sabalete Rodríguez <dsabalete@gmail.com>
2023-07-22 17:09:14 +0200

    setup fly.io and render PaaS

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,96 +1,84 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
-require('dotenv').config()
-const Note = require('./models/note')
+const app = express()
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2022-01-10T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2022-01-10T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2022-01-10T19:20:14.298Z',
+    important: true
+  }
+]
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes.map((note) => note.toJSON()))
-  })
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
 })
 
-app.post('/api/notes', (request, response, next) => {
-  const { body } = request
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
+  return maxId + 1
+}
 
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
   }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date()
-  })
-
-  note
-    .save()
-    .then((savedNote) => savedNote.toJSON())
-    .then((savedAndFormattedNote) => {
-      response.json(savedAndFormattedNote)
-    })
-    .catch((error) => next(error))
-})
-
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note.toJSON())
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch((error) => next(error))
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const { body } = request
 
   const note = {
     content: body.content,
-    important: body.important
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
-      response.json(updatedNote.toJSON())
-    })
-    .catch((error) => next(error))
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
-app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch((error) => next(error))
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter((note) => note.id !== id)
 
-app.use(unknownEndpoint)
+  response.status(204).end()
+})
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find((note) => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
   }
-  next(error)
-}
+})
 
-app.use(errorHandler)
-
-const PORT = process.env.PORT
-
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
