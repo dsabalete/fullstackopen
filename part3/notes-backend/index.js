aa83556b0f28403aadc8f9c226c9cf0239c8ec69
David Sabalete Rodríguez <dsabalete@gmail.com>
2023-07-27 02:40:26 +0200

    error handling

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -29,17 +29,8 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
-// app.get('/', (req, res) => {
-//   res.send('<h1>Hello World!</h1>')
-// })
-
-// const generateId = () => {
-//   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
-//   return maxId + 1
-// }
-
 app.post('/api/notes', (request, response) => {
-  const body = request.body
+  const { body } = request
 
   if (body.content === undefined) {
     return response.status(400).json({ error: 'content missing' })
@@ -62,16 +53,25 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
+  const { id } = request.params
   notes = notes.filter((note) => note.id !== id)
 
   response.status(204).end()
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note)
-  })
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 const PORT = process.env.PORT
