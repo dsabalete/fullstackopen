66099cac1c6545a0b5942eb80a3d6bd45f671148
David Sabalete Rodríguez <dsabalete@gmail.com>
2023-07-27 10:38:56 +0200

    phonebook database step5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -43,29 +43,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523'
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122'
-  }
-]
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
     response.json(persons)
@@ -114,6 +91,24 @@ app.delete('/api/persons/:id', (request, response) => {
       console.log(error)
       response.status(400).send({ error: 'malformatted id' })
     })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { id } = request.params
+  const { body } = request
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(id, person, {
+    new: true
+  })
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
 })
 
 app.use(unknownEndpoint)
