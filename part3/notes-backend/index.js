403eb530b5deea0fb38ff240dd1acf63460097d3
David Sabalete Rodríguez <dsabalete@gmail.com>
2023-07-27 02:46:04 +0200

    move error handling into middleware

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -29,6 +29,19 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
 app.post('/api/notes', (request, response) => {
   const { body } = request
 
@@ -59,7 +72,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
@@ -68,10 +81,7 @@ app.get('/api/notes/:id', (request, response) => {
         response.status(404).end()
       }
     })
-    .catch((error) => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' })
-    })
+    .catch((error) => next(error))
 })
 
 const PORT = process.env.PORT
