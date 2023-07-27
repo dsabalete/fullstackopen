5f82ed3d47b02d34a3406c0946642cdf312ed559
David Sabalete Rodríguez <dsabalete@gmail.com>
2023-07-27 03:11:53 +0200

    other operations

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -2,32 +2,41 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+
 const Note = require('./models/note')
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2022-01-10T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2022-01-10T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2022-01-10T19:20:14.298Z',
-    important: true
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
-]
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(cors())
-app.use(express.static('build'))
 app.use(express.json())
+app.use(requestLogger)
+app.use(express.static('build'))
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes)
+  })
+})
 
 app.post('/api/notes', (request, response) => {
   const { body } = request
@@ -46,19 +55,6 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes)
-  })
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const { id } = request.params
-  notes = notes.filter((note) => note.id !== id)
-
-  response.status(204).end()
-})
-
 app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
@@ -71,23 +67,31 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch((error) => next(error))
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+app.delete('/api/notes/:id', (request, response) => {
+  const { id } = request.params
+  Note.findByIdAndRemove(id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
 
-app.use(unknownEndpoint)
+app.put('/api/notes/:id', (request, response, next) => {
+  const { body } = request
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+  const note = {
+    content: body.content,
+    important: body.important
   }
 
-  next(error)
-}
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote)
+    })
+    .catch((error) => next(error))
+})
 
-// this has to be the last loaded middleware.
+app.use(unknownEndpoint)
 app.use(errorHandler)
 
 const PORT = process.env.PORT
